eca249067a039e2e14f188c08ec58d1a7d7b7339
alishashamoun <alishashamoun12345@gmail.com>
2025-05-20 21:55:38 +0500

    Updated app.blade.php layout

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -195,7 +195,6 @@
 
 
 <script>
-
     $('.main-caro').owlCarousel({
         loop: true,
         navText: ["<i class='fa-solid fa-chevron-left'></i>", "<i class='fa-solid fa-chevron-right'></i>"],
